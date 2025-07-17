1713eb0a0a6ee30f7e63fcfac55024d4679f5b0c
Nguyễn Thái Hòa <hoa41300@gmail.com>
2025-07-17 15:25:07 +0700

    Add Docker Container Support. Change Service Host Names.

--- src/RustRetail.APIGateway/Program.cs
+++ src/RustRetail.APIGateway/Program.cs
@@ -3,6 +3,16 @@ using RustRetail.APIGateway.Configuration;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationServices(builder.Configuration);
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(8080);
+    options.ListenAnyIP(8081, listenOption =>
+    {
+        listenOption.UseHttps(
+            builder.Configuration.GetValue<string>("HttpsCert:FileName")!,
+            builder.Configuration.GetValue<string>("HttpsCert:Password")!);
+    });
+});
 
 var app = builder.Build();
 
