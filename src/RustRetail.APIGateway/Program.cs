13dc483f9a7104db44a91e3d655b09db527efe14
Nguyễn Thái Hòa <hoa41300@gmail.com>
2025-09-03 17:09:11 +0700

    Update Application Configuration
    Remove https configuration. Update docker configs.

--- src/RustRetail.APIGateway/Program.cs
+++ src/RustRetail.APIGateway/Program.cs
@@ -3,16 +3,6 @@ using RustRetail.APIGateway.Configuration;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationServices(builder.Configuration);
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.ListenAnyIP(5000);
-    options.ListenAnyIP(5001, listenOption =>
-    {
-        listenOption.UseHttps(
-            builder.Configuration.GetValue<string>("HttpsCert:FileName")!,
-            builder.Configuration.GetValue<string>("HttpsCert:Password")!);
-    });
-});
 
 var app = builder.Build();
 
